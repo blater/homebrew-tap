2a07b8f0cd1a503010be34b8985ec487e2d21de2
blater <blater@users.noreply.github.com>
2026-07-24 10:13:21 +0100

    nestql 1.0.1

--- Formula/nestql.rb
+++ Formula/nestql.rb
@@ -1,9 +1,9 @@
 class Nestql < Formula
   desc "SQL-like language for querying and transforming structured files"
   homepage "https://github.com/blater/nestql"
-  url "https://github.com/blater/nestql/releases/download/v1.0.0/nestql-1.0.0-darwin-arm64.tar.gz"
-  version "1.0.0"
-  sha256 "27bb11c2a7abc62e764f433f29e82927293267ef6777508daad1aacfbe4f4718"
+  url "https://github.com/blater/nestql/releases/download/v1.0.1/nestql-1.0.1-darwin-arm64.tar.gz"
+  version "1.0.1"
+  sha256 "e78e100f90e061fe8dfc52a2588830e605b7e55a7664efb0d0e20131e0b6e85e"
   license "AGPL-3.0-only"
 
   depends_on arch: :arm64
