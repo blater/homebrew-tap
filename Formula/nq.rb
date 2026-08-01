71a5914501f4263f75800eb28fc8fc25fee94acd
blater <blater@users.noreply.github.com>
2026-08-01 13:34:02 +0000

    nq 0.9.6

--- Formula/nq.rb
+++ Formula/nq.rb
@@ -1,9 +1,9 @@
 class Nq < Formula
   desc "SQL-like language for querying and transforming structured files"
   homepage "https://github.com/blater/nq"
-  url "https://github.com/blater/nq/releases/download/v0.9.3/nq-0.9.3-darwin-arm64.tar.gz"
-  version "0.9.3"
-  sha256 "ae4268c859756143be365e8740b608f1888bd5707506bd77cee6ee3b5e32fdc7"
+  url "https://github.com/blater/nq/releases/download/v0.9.6/nq-0.9.6-darwin-arm64.tar.gz"
+  version "0.9.6"
+  sha256 "d5adf00e242138f3d9051398f0a469ca464a456b2d2ae06d2134652101e0eb18"
   license "AGPL-3.0-only"
 
   depends_on arch: :arm64
