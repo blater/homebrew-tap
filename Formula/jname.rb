61994b3910236126b69c07b28934618537ec9d2a
blater <blater@users.noreply.github.com>
2026-09-14 10:50:18 +0000

    jname 0.3.0

--- Formula/jname.rb
+++ Formula/jname.rb
@@ -1,17 +1,17 @@
 class Jname < Formula
   desc "Generate human-readable random names"
   homepage "https://github.com/blater/jname"
-  url "https://github.com/blater/jname/releases/download/v0.1.0/jname-0.1.0-darwin-arm64.tar.gz"
-  version "0.1.0"
-  sha256 "a5d275ad221cadd12a4ed163b2f5c1a28c3020a7409afb60792f42bc71672e05"
-  license "Apache-2.0"
+  url "https://github.com/blater/jname/releases/download/v0.3.0/jname-0.3.0-darwin-arm64.tar.gz"
+  version "0.3.0"
+  sha256 "b1fb0b95885dd367111b30219b2228a999a13d39ede2f9ebdccdae9e74df4518"
+  license all_of: ["MIT", "Apache-2.0", "CC-BY-SA-4.0"]
 
   depends_on arch: :arm64
   depends_on macos: :sequoia
 
   def install
     bin.install "jname"
-    pkgshare.install "LICENSE", "NOTICE"
+    pkgshare.install "LICENSE", "NOTICE", "LICENSES"
   end
 
   test do
